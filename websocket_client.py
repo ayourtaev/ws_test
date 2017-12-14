db204840f1f60809f50cc3b0f3cca404e78d99eb
igloo <foo@foo.bar>
2017-12-14 12:10:33 +0200

    .gitignore + websocket_client + websocket_server has added

--- websocket_client.py
+++ websocket_client.py
@@ -1,2 +1,15 @@
+import asyncio
+import websockets
+
+async def hello():
+    async with websockets.connect('ws://localhost:8765') as websocket:
+        name = input('What\'s your name? ')
+        await websocket.send(name)
+        print('> {}'.format(name))
+
+        greeting = await websocket.recv()
+        print('< {}'.format(greeting))
+
 if __name__ == '__main__':
     print('Hello im ws client')
+    asyncio.get_event_loop().run_until_complete(hello())
--- websocket_server.py
+++ websocket_server.py
@@ -1,2 +1,21 @@
+import asyncio
+import websockets
+
+
+async def hello(websocket, path):
+    # Here’s a WebSocket server example. It reads a name from the client,
+    # sends a greeting, and closes the connection.
+    name = await websocket.recv()
+    print('< {}'.format(name))
+
+    greeting = 'Hello {}!'.format(name)
+    await  websocket.send(greeting)
+    print('> {}'.format(greeting))
+
+
 if __name__ == '__main__':
     print('Hello im ws server')
+    start_server = websockets.serve(hello, 'localhost', 8765)
+
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
